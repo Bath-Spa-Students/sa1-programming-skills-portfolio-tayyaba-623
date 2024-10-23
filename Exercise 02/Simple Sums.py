a05e9d9a3bf14e051c4a39d905ca444a1c726b94
Tayyaba <ttayyaba287@gmail.com>
2024-10-23 02:00:43 -0700

    Simple sums.py

--- Exercise 02/Simple Sums.py
+++ Exercise 02/Simple Sums.py
@@ -7,8 +7,13 @@ Declare a third variable that stores the sum of first two numbers.
 Print the value of the sum to the console.
 """
 
-#performing arithmetic operations
+
+#Declare first varaible and intialize it with the value 8
 x= 8
+#Declare second variable and intialize it with the value 10
 y= 10
+#Declare third varaible that stores the sum of first two numbers
 z= x+y
+#print the value of the sum
 print(f" The sum of x and y is {z}")
+
