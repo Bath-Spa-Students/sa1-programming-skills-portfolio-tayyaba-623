c4d7a3b0529a5fe63e4fa56bdd9fc672866378e8
Tayyaba <ttayyaba287@gmail.com>
2024-11-09 00:57:38 -0800

    Biography Advance.py

---
+++ Exercise 03/Biography Advance.py
@@ -0,0 +1,31 @@
+"""
+## Exercise 3: Biography - 25 Marks
+
+In this exercise, you'll create a program that stores and prints your name, hometown, and age to the console using a Python dictionary.
+
+### Steps:
+1. Store the information (name, hometown, and age) as key-value pairs in a dictionary.
+2. Print the values on separate lines using a single `print()` statement.
+3. Use variables with appropriate data types for each piece of information.
+
+
+
+### Advanced Requirements:
+Have the user input their name, hometown, and age instead of hardcoding the values.
+Try giving both your first and second name when asked for your name. What happens? How can you handle multiple words in Python?
+Test the program by entering a string value for age (e.g., "twenty"). What happens? How can you prevent this issue?
+
+"""
+#Get input from the user
+name = input("Enter your name:")
+hometown = input("Enter your hometown:")
+#While loop
+while True:
+    age = input("Enter your age:")
+#if statement
+    if age. isdigit():
+       age = int(age)
+       break
+    else: 
+         print("Age should be an integer")
+      
