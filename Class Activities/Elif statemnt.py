3d20142e4097130ed9af39859e49c370675a2894
Tayyaba <ttayyaba287@gmail.com>
2024-11-19 04:28:28 -0800

    Elif statemnt.py

--- Class Activities/Elif statemnt.py
+++ Class Activities/Elif statemnt.py
@@ -1,24 +1,24 @@
 # Elif statement
-c=55
-d=7
-# if-elif Statement
-if c<d:
+c = 55
+d = 7
+if-elif-else statement
+if c < d:
     print("C is less than d")
-elif c==d:
+elif c == d:
     print("C and d are equal")  
 else:
     print("C is greater than d")
       
-# To get input from the user
+# Taking input from the user
 score= int(input("Enter your scores:"))
-# if-elif statement
-if score>=90:
+# if-elif-else statement
+if score >= 90:
     print("Your grade is A.")
-elif score>=80:
+elif score >= 80:
     print("Your grade is B.")
-elif score>=70:
+elif score >= 70:
     print("Your grade is C.")
-elif score>=50:
+elif score >= 50:
     print("Yoyr grade is D.")
 else:
     print("Your grade is F.")
