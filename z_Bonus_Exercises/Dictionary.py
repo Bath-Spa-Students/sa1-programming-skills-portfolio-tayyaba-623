701647db8e93543f0ebda15890988c7f2e736b8a
Tayyaba <ttayyaba287@gmail.com>
2024-11-19 01:36:02 -0800

    Dictionary.py

---
+++ z_Bonus_Exercises/Dictionary.py
@@ -0,0 +1,15 @@
+"""
+Dictionaries:
+Use a dictionary to store information about a person you know.Store their first name,
+last name, age, and the city in which they live.
+
+You should have keys such as first_name, last_name, age, and city. Print each piece of
+information stored in your dictionary.
+"""
+# Storing information in dictionary
+Information = {"First_name" : "Naila" , "Last_name" : "Akhtar" , "Age" : "18" , "City" : "Rak AL Khaimah" }
+# Printing the information
+print("First name:", Information["First_name"])
+print("Last name:" , Information["Last_name"])
+print("Age:" , Information ["Age"])
+print("City:" , Information ["City"])
