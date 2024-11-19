07170b914d20fb4a096f70d85eab0cd809db867c
Tayyaba <ttayyaba287@gmail.com>
2024-11-19 01:45:48 -0800

    Function.py

---
+++ z_Bonus_Exercises/Function.py
@@ -0,0 +1,14 @@
+"""
+Functions -Favorite Book:
+Write a function called favorite_book() that accepts one parameter, title. The function
+should print a message, such as One of my
+favorite books is Alice in Wonderland. Call the function, making sure to include a book
+title as an argument in the function call.
+"""
+
+# Functions
+def favorite_book(Title):
+    print("One of my favorite book is: ", Title)
+# Calling the function
+favorite_book("Beauty and the Beast")
+
