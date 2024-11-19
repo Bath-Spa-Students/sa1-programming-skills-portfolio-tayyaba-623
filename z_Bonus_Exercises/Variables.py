1164e071db64391938aeaa0a79ef108e5a47e08c
Tayyaba <ttayyaba287@gmail.com>
2024-11-19 01:32:25 -0800

    Variables.py

---
+++ z_Bonus_Exercises/Variables.py
@@ -0,0 +1,12 @@
+"""
+Variables:
+Assign a message to a variable, and print that message. Then change the value of the
+variable to a new message, and print the new message.
+"""
+# First variable 
+Message_1 = "Tayyaba Qamar" 
+# Print the variable
+print(Message_1)
+# Second variable
+Message_2 = "Creative Computing"
+print(Message_2)    # Print the variable
