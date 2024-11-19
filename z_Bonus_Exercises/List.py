3f06e8ace11ae5af8973164950b224c7c3edde6e
Tayyaba <ttayyaba287@gmail.com>
2024-11-19 01:39:39 -0800

    List.py

---
+++ z_Bonus_Exercises/List.py
@@ -0,0 +1,12 @@
+"""
+List -Names
+Store the names of a few of your friends in a list called names. Print each person’s name
+by accessing each element in the list, one at a time.
+"""
+# Storing names in list
+Names = ["Naila", "Arfa", "Ayesha", "Haya"]
+# Printing all the names 
+print(Names[0])
+print(Names[1])
+print(Names[2])
+print(Names[3])
