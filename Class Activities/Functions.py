c95d1b51e3dc74297d2f6016ec62242a495f4240
Tayyaba <ttayyaba287@gmail.com>
2024-11-20 03:49:40 -0800

    Functions.py

---
+++ Class Activities/Functions.py
@@ -0,0 +1,17 @@
+# Functions
+def print_message_1():
+    message = "Tayyaba"
+    print(message)
+
+def print_message_2():
+    message = "Naila"
+    print(message)
+
+def print_message_3():
+    message = "Hello"
+    print(message)
+  
+# Calling the functions
+print_message_1()
+print_message_2()
+print_message_3()
