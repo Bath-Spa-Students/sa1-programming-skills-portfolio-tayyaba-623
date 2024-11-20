8045eda3d75ee0a903270ce12a6ccf7d11a5423e
Tayyaba <ttayyaba287@gmail.com>
2024-11-20 05:48:15 -0800

    for loop.py

--- Class Activities/for loop.py
+++ Class Activities/for loop.py
@@ -1,5 +1,5 @@
 # for loop
-Names= ["tayyaba", "naila"]
+Names = ["Tayyaba", "Naila"]
 for i in Names:
 # print the variable
     print(i)
@@ -21,6 +21,6 @@ for n in range(0,45,7):
     print(n, end=",")
 
 # user controlled loop
-t= int(input("Enter the mximum range:"))
+t = int(input("Enter the mximum range:"))
 for t in range(0,18,4):
     print(t, end=",")
