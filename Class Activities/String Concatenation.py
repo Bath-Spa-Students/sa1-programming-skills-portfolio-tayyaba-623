3b3befa4d4193f06282ed6f962797f680eba744b
Tayyaba <ttayyaba287@gmail.com>
2024-11-20 05:46:08 -0800

    String Concatenation.py

--- Class Activities/String Concatenation.py
+++ Class Activities/String Concatenation.py
@@ -1,24 +1,24 @@
 # string concatenation
-name="tele"+"vision"
+name  = "tele"+"vision"
 # print name
 print(name)
 
 # string concatenation through print 
-print("ice"+"cream")
+print("ice" + "cream")
 
 # string concatenation 
 # First variable
-b='i want to'
+b = 'I want to'
 # Second variable
-c= ' drive a car'
+c = ' drive a car'
 # print b and c
 print(b + "" + c)
 
 # Another example
 # First variable
-age= "17"
+age = "17"
 # Second variable
-height= " 5.2"
+height = " 5.2"
 # print both variables
 print(age + "" + height)
 
