8a195d2d3c85d563e2d6b6ff7a2ec67aeaa4bdaf
Tayyaba <ttayyaba287@gmail.com>
2024-11-20 05:49:01 -0800

    if else statement.py

--- Class Activities/if else statement.py
+++ Class Activities/if else statement.py
@@ -1,14 +1,14 @@
 # if statement
-c= 9
-d= 3
+c = 9
+d = 3
 # if Statement
 if c > d:
     print("c is greater than d")
 
 
 # if-else statement
-y= 6
-z= 9
+y = 6
+z = 9
 if y > z:
     print("y is greater than z")
 else:
