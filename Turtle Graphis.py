48461e5fdeebba2f8e3ca6282de724335fa3ec3c
Tayyaba <ttayyaba287@gmail.com>
2024-11-20 05:24:26 -0800

    Turtle Graphis.py

---
+++ Turtle Graphis.py
@@ -0,0 +1,12 @@
+# Turtle Graphis
+import turtle
+t = turtle.Turtle()
+# Background color
+turtle.bgcolor ("White")
+colors = ["Blue" , "Dark blue"] 
+# for loop
+for x in range(400):
+    t. forward (x + 1)
+    t. right (89)
+    t. pencolor (colors [x % 2])
+turtle. done () 
